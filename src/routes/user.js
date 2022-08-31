c217f56500a2081f9ba88304d9ef197136f92fdb
Pablo costa <snoop.gz@gmail.com>
2022-08-31 16:22:15 -0300

    basic functions

---
+++ src/routes/user.js
@@ -0,0 +1,9 @@
+let express = require("express");
+let router = express.Router();
+let simpleResponse = require("../middleware/simpleresponse");
+
+router.get("/", simpleResponse);
+router.post("/", simpleResponse);
+router.patch("/", simpleResponse);
+
+module.exports = router;
