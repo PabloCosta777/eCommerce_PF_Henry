678158da7c1826eded391993bc278f5f96d36319
F. Andrés Villegas <avillegas332@gmail.com>
2022-09-02 11:44:03 -0300

    ruta /product

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,8 +2,9 @@ let express = require("express");
 let router = express.Router() ;
 let simpleResponse =require('../middleware/simpleresponse')   
 let postProduct = require('../Controllers/postProduct')
+const getProductDB = require("../Controllers/DBgetProducts.js")
 
-router.get('/',  simpleResponse)
+router.get('/',  getProductDB)
 router.post('/', postProduct)
 router.patch('/',simpleResponse)
 
