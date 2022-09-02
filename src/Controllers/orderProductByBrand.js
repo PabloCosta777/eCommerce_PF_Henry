8ef68518cf9bd920ee72242a37e82778cde95e77
IvanRomano <ivan.ac.romano@gmail.com>
2022-09-02 17:53:12 -0300

    arreglado filtros y ordenamientos

--- src/Controllers/orderProductByBrand.js
+++ src/Controllers/orderProductByBrand.js
@@ -1,25 +1,25 @@
-const { Product } = require('../db')
+const Product = require('../models/product');
 
-const orderProductByName = async (req, res) => {
-    let nameOrder = req.params.nameOrder;
+const orderProductByBrand = async (req, res) => {
+    let nameOrder = req.body.nameOrder;
     const allProducts = await Product.findAll()
 
     try {
         nameOrder === 'A-Z' ?
             allProducts.sort(function (a, b) {
-                if (a.name > b.name) {
+                if (a.marca > b.marca) {
                     return 1;
                 }
-                if (b.name > a.name) {
+                if (b.marca > a.marca) {
                     return -1;
                 }
                 return 0;
             }) :
             allProducts.sort(function (a, b) {
-                if (a.name > b.name) {
+                if (a.marca > b.marca) {
                     return -1;
                 }
-                if (b.name > a.name) {
+                if (b.marca > a.marca) {
                     return 1;
                 }
                 return 0;
@@ -30,7 +30,6 @@ const orderProductByName = async (req, res) => {
     }
 }
 
-module.exports = {
-    orderProductByName
-}
+module.exports = orderProductByBrand;
+
 
--- src/routes/product.js
+++ src/routes/product.js
@@ -3,9 +3,14 @@ let router = express.Router() ;
 let simpleResponse =require('../middleware/simpleresponse')   
 let postProduct = require('../Controllers/postProduct')
 const getProductDB = require("../Controllers/DBgetProducts.js")
+let orderProductByBrand = require('../Controllers/orderProductByBrand')
+let orderByPrice = require("../Controllers/orderProductByPrice")
 
 router.get('/',  getProductDB)
 router.post('/', postProduct)
 router.patch('/',simpleResponse)
+router.get('/orderName', orderProductByBrand )
+router.get('/orderPrice', orderByPrice )
+
 
 module.exports = router;
